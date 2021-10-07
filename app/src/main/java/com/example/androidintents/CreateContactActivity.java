2c27e8173c4f8b13dc0b74b014fa7e11eb22bbac
Frank David Addae <frankdavid.addae@gmail.com>
2021-10-07 15:02:39 +0000

    completed

--- app/src/main/java/com/example/androidintents/CreateContactActivity.java
+++ app/src/main/java/com/example/androidintents/CreateContactActivity.java
@@ -2,13 +2,59 @@ package com.example.androidintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class CreateContactActivity extends AppCompatActivity {
+public class CreateContactActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etName, etPhoneNumber, etWebAddress, etLocation;
+    ImageView ivHappy, ivNeutral, ivSad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_contact);
+
+        etName = findViewById(R.id.etName);
+        etPhoneNumber = findViewById(R.id.etPhoneNumber);
+        etWebAddress = findViewById(R.id.etWebAddress);
+        etLocation = findViewById(R.id.etLocation);
+
+        ivHappy = findViewById(R.id.ivHappy);
+        ivNeutral = findViewById(R.id.ivNeutral);
+        ivSad = findViewById(R.id.ivSad);
+
+        ivHappy.setOnClickListener(this);
+        ivNeutral.setOnClickListener(this);
+        ivSad.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (etName.getText().toString().isEmpty() || etPhoneNumber.getText().toString().isEmpty() ||
+                etWebAddress.getText().toString().isEmpty() || etLocation.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Please enter all fields!", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent();
+            intent.putExtra("name", etName.getText().toString().trim());
+            intent.putExtra("phoneNumber", etPhoneNumber.getText().toString().trim());
+            intent.putExtra("webAddress", etWebAddress.getText().toString().trim());
+            intent.putExtra("location", etLocation.getText().toString().trim());
+
+            if (view.getId() == R.id.ivHappy) {
+                intent.putExtra("mood", "happy");
+            } else if (view.getId() == R.id.ivNeutral) {
+                intent.putExtra("mood", "neutral");
+            } else {
+                intent.putExtra("mood", "sad");
+            }
+
+            setResult(RESULT_OK, intent);
+            CreateContactActivity.this.finish();
+        }
     }
 }
--- app/src/main/java/com/example/androidintents/MainActivity.java
+++ app/src/main/java/com/example/androidintents/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.androidintents;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView ivMood, ivPhone, ivWeb, ivLocation;
     Button btnCreateContact;
+    final int CREATE_CONTACT = 1;
+    String name = "", phoneNumber = "", webAddress = "", location = "", mood = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,31 +38,69 @@ public class MainActivity extends AppCompatActivity {
         ivWeb.setVisibility(View.GONE);
         ivLocation.setVisibility(View.GONE);
 
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+
+                            ivMood.setVisibility(View.VISIBLE);
+                            ivPhone.setVisibility(View.VISIBLE);
+                            ivWeb.setVisibility(View.VISIBLE);
+                            ivLocation.setVisibility(View.VISIBLE);
+
+                            name = data.getStringExtra("name");
+                            phoneNumber = data.getStringExtra("phoneNumber");
+                            webAddress = data.getStringExtra("webAddress");
+                            location = data.getStringExtra("location");
+                            mood = data.getStringExtra("mood");
+
+                            if (mood.equals("happy")) {
+                                ivMood.setImageResource(R.drawable.ic_happy);
+                            } else if (mood.equals("neutral")) {
+                                ivMood.setImageResource(R.drawable.ic_neutral);
+                            } else {
+                                ivMood.setImageResource(R.drawable.ic_sad);
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, "No data was received!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
         btnCreateContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this,
+                        com.example.androidintents.CreateContactActivity.class);
+                activityResultLauncher.launch(intent);
             }
         });
 
         ivPhone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+                startActivity(intent);
             }
         });
 
         ivWeb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://" + webAddress));
+                startActivity(intent);
             }
         });
 
         ivLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + location));
+                startActivity(intent);
             }
         });
     }
