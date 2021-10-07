0cb634846834427f67aeb6bc8655316fa2389629
Frank David Addae <frankdavid.addae@gmail.com>
2021-10-07 13:20:05 +0000

    adding new contact logic

--- app/src/main/java/com/example/androidintents/MainActivity.java
+++ app/src/main/java/com/example/androidintents/MainActivity.java
@@ -3,12 +3,57 @@ package com.example.androidintents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivMood, ivPhone, ivWeb, ivLocation;
+    Button btnCreateContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivMood = findViewById(R.id.ivMood);
+        ivPhone = findViewById(R.id.ivPhone);
+        ivWeb = findViewById(R.id.ivWeb);
+        ivLocation = findViewById(R.id.ivLocation);
+        btnCreateContact = findViewById(R.id.btnCreateContact);
+
+        ivMood.setVisibility(View.GONE);
+        ivPhone.setVisibility(View.GONE);
+        ivWeb.setVisibility(View.GONE);
+        ivLocation.setVisibility(View.GONE);
+
+        btnCreateContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        ivPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        ivWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        ivLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
